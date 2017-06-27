1841ad1d69c3125ff7bfe21579b2792427815b72
James Schumann <jschumann@gmail.com>
2017-06-27 18:42:15 +0000

    stubbing out logic for tranformative function

--- people_inbound/index.js
+++ people_inbound/index.js
@@ -1,7 +1,39 @@
 module.exports = function (context, message) {
     context.log(context);
 
-    context.bindings.document = context.bindings.message;
+    // get our person by id
+    // var person = context.bindings.personRecordIn;
+
+    // update username if changed
+    // update email if changed
+    // update name if changed
+    // update sortable name if changed
+    // update first_name if changed
+    // update last_name if changed
+
+    // create a new var to hold assignment from message
+    // var ipps_assignment;
+
+    // add fields to assignment
+    //"ipps_activity_code": "ACTIVE",
+    //"ipps_employee_group_category": "OCC-STCHR",
+    //"ipps_employee_group_code": "5132S",
+    //"ipps_employee_group_description": "Secondary Occasional Teacher",
+    //"ipps_extension": null,
+    //"ipps_job_code": "8125",
+    //"ipps_job_description": "Occasional - Secondary",
+    //"ipps_location_code": "999",
+    //"ipps_location_description": "Payroll - Distr",
+    //"ipps_panel": null,
+    //"ipps_phone_no": null,
+    //"ipps_school_code": "999",
+    //"ipps_school_type": "BU",
+    //"ipps_home_location_indicator": "Y"
+
+    //  compare this assigment with those already on file
+
+    // context.bindings.document = context.bindings.message;
+    // context.bindings.personRecordOut = context.bindings.personRecordIn;
 
     context.done();
 };

50afbc6339eacded16feb9853d3b510e2a8f2e45
James Schumann <jschumann@gmail.com>
2017-10-04 16:21:03 +0000

    rip out old functions

--- google_calendars_inbound/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (context, message) {
-    context.log(context);
-
-    context.bindings.document = context.bindings.message;
-
-    context.done();
-};
--- people_changes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (context, message) {
-    context.log(context);
-
-    context.bindings.change = message;
-
-    context.done();
-};

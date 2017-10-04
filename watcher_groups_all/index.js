0ebb95f2a780ec70c5ae45a0dad2c26fce644f7f
James Schumann <jschumann@gmail.com>
2017-10-04 16:56:47 +0000

    add throttling to watcher_groups_all

--- watcher_groups_all/index.js
+++ watcher_groups_all/index.js
@@ -1,12 +1,31 @@
 module.exports = function (context) {
+    var moment = require('moment');
+    var offset = 0;
+    var scheduledTime;
+
     // give our bindings more human-readable names
     var groups_all = context.bindings.groupsAll;
     var groups_to_read = [];
 
     Object.getOwnPropertyNames(groups_all).forEach(function (group) {
-        context.log('Requesting refresh of ' + group);
-        groups_to_read.push(group);
+        var messageBody = {
+            groupKey: group
+        };
+
+        scheduledTime = moment().utc().add(offset, 'minute').format('M/D/YYYY H:mm:ss A');
+        
+        var message = {
+            body: JSON.stringify(messageBody),
+            brokerProperties: {
+                ScheduledEnqueueTimeUtc: scheduledTime
+            }
+        };
+
+        context.log('Requesting refresh of '+ group +' at '+ scheduledTime +' UTC.');
+        groups_to_read.push(message);
+
+        offset = offset + 0.1;
     });
-    context.bindings.groupsToRead = [];
+    context.bindings.groupsToRead = groups_to_read;
     context.done();
 };

7799b211fa1bbc1bf96ba97bb5d180a4462c1d51
James Schumann <jschumann@gmail.com>
2017-12-13 14:51:12 +0000

    let's try an update

--- groups_group_patch/index.js
+++ groups_group_patch/index.js
@@ -1,12 +1,23 @@
 module.exports = function (context, data) {
     context.log(data);
 
-    // get the current record
+    // Fail if data does not include email
+
+    // Get the current record from Codex
     var current_record = context.bindings.codexGroup;
     context.log(current_record);
+
+    // We use the Group's email address as the Cosmos DB record's id
+    // and store the 'real' id as google_id.
+    // So, if we have an id, and it's not an email address,
+    // move it into the google_id, and replace it with the email address
+    if (data.id && data.indexOf('@') == -1) {
+        data.google_id = data.id;
+        data.id = data.email;
+    }
     
-    // merge request object into current record and save
-    //context.bindings.codexGroup = Object.assign(current_record, data);
+    // Merge request object into current record and save
+    context.bindings.codexGroup = Object.assign(current_record, data);
 
     context.done();
 };

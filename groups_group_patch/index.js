8c4d261d492654d5af31d42aa8e72566afd97b7c
James Schumann <jschumann@gmail.com>
2017-12-13 15:17:03 +0000

    watch out for missing current record

--- groups_group_patch/index.js
+++ groups_group_patch/index.js
@@ -1,11 +1,10 @@
 module.exports = function (context, data) {
+    var current_record;
+    var merged_record;
+
     context.log(data);
 
     // Fail if data does not include email
-
-    // Get the current record from Codex
-    var current_record = context.bindings.codexGroupIn;
-    context.log(current_record);
 
     // We use the Group's email address as the Cosmos DB record's id
     // and store the 'real' id as google_id.
@@ -16,8 +15,16 @@ module.exports = function (context, data) {
         data.id = data.email;
     }
     
-    // Merge request object into current record
-    var merged_record = Object.assign(current_record, data);
+    // Get the current record from Codex
+    current_record = context.bindings.codexGroupIn;
+    context.log(current_record);
+
+    if (current_record) {
+        // Merge request object into current record
+        merged_record = Object.assign(current_record, data);
+    } else {
+        merged_record = data;
+    }
     context.log(merged_record);
 
     context.bindings.codexGroupOut = merged_record;

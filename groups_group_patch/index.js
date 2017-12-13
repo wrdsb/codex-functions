7e8783c77a3b75443ca540232d74479f682c1ac3
James Schumann <jschumann@gmail.com>
2017-12-13 14:06:13 +0000

    stub groups_group_patch

---
+++ groups_group_patch/index.js
@@ -0,0 +1,9 @@
+module.exports = function (context, data) {
+    context.log(data);
+
+    // get the current record
+    
+    // merge request object into current record
+    
+    // save current record
+};

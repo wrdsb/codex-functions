bb6ab7d50eaa77f1b9f5f2052c943eaac81c4c9b
James Schumann <jschumann@gmail.com>
2017-12-13 14:23:18 +0000

    working out input binding

--- groups_group_patch/index.js
+++ groups_group_patch/index.js
@@ -2,8 +2,11 @@ module.exports = function (context, data) {
     context.log(data);
 
     // get the current record
+    var current_record = context.bindings.codexGroup;
+    context.log(current_record);
     
-    // merge request object into current record
-    
-    // save current record
+    // merge request object into current record and save
+    //context.bindings.codexGroup = Object.assign(current_record, data);
+
+    context.done();
 };

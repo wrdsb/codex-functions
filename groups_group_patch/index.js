bf23b7c8948177c32c0628e698515fee2f9e26a7
James Schumann <jschumann@gmail.com>
2018-01-20 14:53:43 +0000

    fix bug id check

--- groups_group_patch/index.js
+++ groups_group_patch/index.js
@@ -4,13 +4,13 @@ module.exports = function (context, data) {
 
     context.log(data);
 
-    // Fail if data does not include email
+    // TODO: Fail if data does not include email
 
     // We use the Group's email address as the Cosmos DB record's id
     // and store the 'real' id as google_id.
     // So, if we have an id, and it's not an email address,
     // move it into the google_id, and replace it with the email address
-    if (data.id && data.indexOf('@') == -1) {
+    if (data.id && data.id.indexOf('@') == -1) {
         data.google_id = data.id;
         data.id = data.email;
     }

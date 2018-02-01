bbed796510b1738e9b03c8b38e411636017dc80f
James Schumann <jschumann@gmail.com>
2018-02-01 19:41:38 +0000

    add ipps_person_read

---
+++ ipps_person_read/index.js
@@ -0,0 +1,28 @@
+module.exports = function (context, data) {
+    var current_record;
+
+    context.log(data);
+
+    // TODO: Fail if data does not include email
+
+    // Get the current record from Codex
+    current_record = context.bindings.codexIPPSPersonIn;
+    context.log(current_record);
+
+    if (current_record) {
+        context.log(current_record);
+        context.res = {
+            status: 200,
+            body: current_record
+        };
+        context.done(null, JSON.stringify(current_record));
+        return;
+    } else {
+        context.res = {
+            status: 404,
+            body: "Not found."
+        };
+        context.done('404', 'Not found.');
+        return;
+    }
+};

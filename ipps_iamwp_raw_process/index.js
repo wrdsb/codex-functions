40acb913bd573c9c037e10d5b67e255be93723b5
James Schumann <jschumann@gmail.com>
2018-03-13 12:05:39 +0000

    output jobs, groups, and locations blobs

--- ipps_iamwp_raw_process/index.js
+++ ipps_iamwp_raw_process/index.js
@@ -1,9 +1,17 @@
 module.exports = function (context, data) {
     var rows = context.bindings.iamwpRaw;
+
     var peopleObject = {};
     var peopleArray = [];
+    var jobsObject = {};
+    var jobsArray = {};
+    var groupsObject = {};
+    var groupsArray = {};
+    var locationsObject = {};
+    var locationsArray = {};
 
     rows.forEach(function(row) {
+        // Create the main part of a Person object
         var personRecord = {
             id:             row.ipps_ein,
             ein:            row.ipps_ein,
@@ -15,6 +23,7 @@ module.exports = function (context, data) {
             email:          row.email
         };
 
+        // Create the Position object for this row
         var personPosition = {
             ipps_ein:                         row.ipps_ein,
             ipps_activity_code:               row.ipps_activity_code,
@@ -36,9 +45,11 @@ module.exports = function (context, data) {
             ipps_position_end_date:           row.ipps_position_end_date
         };
 
+        // Grab what will become our object identifiers
         var ein = row.ipps_ein;
         var position_id = row.ipps_position_id;
 
+        // Upsert Person, and current Position, to people collection object
         if (peopleObject[ein]) {
             if (personPosition.ipps_home_location_indicator === 'Y') {peopleObject[ein].ipps_home_location = personPosition.ipps_location_code;}
             peopleObject[ein].positions[position_id] = personPosition;
@@ -48,8 +59,24 @@ module.exports = function (context, data) {
             personRecord.positions[position_id] = personPosition;
             peopleObject[ein] = personRecord;
         }
+
+        // Add/overwrite jobs, groups, and locations from this row to their collection objects
+        jobsObject[row.ipps_job_code] = {
+            job_code: row.ipps_job_code,
+            job_description: row.ipps_job_description,
+        };
+        groupsObject[row.ipps_employee_group_code] = {
+            employee_group_category: row.ipps_employee_group_category,
+            employee_group_code: row.ipps_employee_group_code,
+            employee_group_description: row.ipps_employee_group_description,
+        };
+        locationsObject[row.ipps_location_code] = {
+            location_code: row.ipps_location_code,
+            location_description: row.ipps_location_description,
+        };
     });
-    
+
+    // Add each person from peopleObject to peopleArray
     Object.getOwnPropertyNames(peopleObject).forEach(function (ein) {
         var person = peopleObject[ein];
         var positions_array = [];
@@ -60,6 +87,22 @@ module.exports = function (context, data) {
         peopleArray.push(person);
     });
 
+    // Step through other collection objects and assign objects to their arrays
+    Object.getOwnPropertyNames(jobsObject).forEach(function (job) {
+        jobsArray.push(jobsObject[job]);
+    });    
+    Object.getOwnPropertyNames(groupsObject).forEach(function (group) {
+        groupsArray.push(groupsObject[group]);
+    });
+    Object.getOwnPropertyNames(locationsObject).forEach(function (location) {
+        locationsArray.push(locationsObject[location]);
+    });
+
+    // Write out arrays to blobs
     context.bindings.peopleNow = JSON.stringify(peopleArray);
+    context.bindings.jobsNow = JSON.stringify(jobsArray);
+    context.bindings.groupsNow = JSON.stringify(groupsArray);
+    context.bindings.locationsNow = JSON.stringify(locationsArray);
+
     context.done();
 };

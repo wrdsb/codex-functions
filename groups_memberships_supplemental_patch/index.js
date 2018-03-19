89ee88a308a666727e7cf02c8f4c67c3f3980414
James Schumann <jschumann@gmail.com>
2018-03-19 15:24:46 +0000

    finish renaming and tidy up groups_memberships_supplemental_patch

--- groups_memberships_supplemental_patch/index.js
+++ groups_memberships_supplemental_patch/index.js
@@ -29,7 +29,7 @@ module.exports = function (context, data) {
     flynn_event = {
         event_type: 'function_invocation',
         app: 'wrdsb-codex',
-        operation: 'groups_memberships_ideal_patch',
+        operation: 'groups_memberships_supplemental_patch',
         function_name: context.executionContext.functionName,
         invocation_id: context.executionContext.invocationId,
         data: {

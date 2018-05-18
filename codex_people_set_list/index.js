652722dce5db9cdc8ba757c45858280d3989202a
James Schumann <jschumann@gmail.com>
2018-05-18 15:40:46 -0400

    output list as both array and object

--- codex_people_set_list/index.js
+++ codex_people_set_list/index.js
@@ -1,6 +1,15 @@
 module.exports = function (context) {
     var people_sets = context.bindings.codexRecordsIn;
-    context.log(people_sets);   
-    context.bindings.codexBlobOutArray = people_sets;
+
+    var people_sets_array = [];
+    var people_sets_object = {};
+    
+    people_sets.forEach(function (set) {
+        people_sets_array.push(set);
+        people_sets_object[set.id] = set;
+    });
+
+    context.bindings.codexBlobOutArray = JSON.stringify(people_sets_array);
+    context.bindings.codexBlobOutObject = JSON.stringify(people_sets_object);
     context.done();
 };
